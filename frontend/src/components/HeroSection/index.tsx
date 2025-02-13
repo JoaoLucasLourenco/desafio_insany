00c9d415ebfd72618d79839ccb43b1c7e8b5b9c2
João Lucas Lourenço <118781114+JoaoLucasLourenco@users.noreply.github.com>
2025-02-13 13:34:27 +0000

    create: hero section básica criada, imagem de fundo adiconada, responsividade da imagem implementada

---
+++ frontend/src/components/HeroSection/index.tsx
@@ -0,0 +1,8 @@
+"use client";
+
+import { Section } from "./style";
+
+const Hero = () => {
+  return <Section></Section>;
+};
+export default Hero;
---
+++ frontend/src/components/HeroSection/style.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Section = styled.section`
+  height: 80vh;
+  background-image: url("maskGroup.png");
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-color: #070514;
+  background-position: center;
+`;
